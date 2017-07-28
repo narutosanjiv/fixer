2e137cae31ba492847a9f1a5759fa3554ae23c8d
kenshin <narutosanjiv@gmail.com>
2017-07-28 13:08:49 +0530

    Added Basic setup for Fixer.io gem

---
+++ lib/fixer.rb
@@ -0,0 +1,11 @@
+require "fixer/version"
+
+module Fixer
+  BaseUrl = "https://api.fixer.io"
+  BaseCurrency = "USD"
+  # Your code goes here...
+
+  def rate_list
+
+  end
+end
---
+++ spec/fixer_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Fixer do
+  it "has a version number" do
+    expect(Fixer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
